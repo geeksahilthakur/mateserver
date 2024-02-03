06d6d57f85852f4ff8b0b183614e6247e7e43518
Sahil Thakur <94735251+geeksahilthakur@users.noreply.github.com>
2024-02-04 01:19:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,5 @@ def delete_item(index):
         return jsonify({"error": "Index not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
