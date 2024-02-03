280ab42356e851b4a2e314609df5431ed562b6fd
Sahil Thakur <94735251+geeksahilthakur@users.noreply.github.com>
2024-02-04 02:05:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def delete_item(index):
         return jsonify({"error": str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
